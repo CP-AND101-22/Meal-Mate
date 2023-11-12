3bc797f1bb38b4b8b51cd60fa0a33cb011696de4
Lynn-Hu-D <lingyuhu0106@gmail.com>
2023-11-12 00:45:45 -0800

    home page

--- Meal-Mate-master/app/src/main/java/com/example/finalprojectapp/MainActivity.kt
+++ Meal-Mate-master/app/src/main/java/com/example/finalprojectapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.finalprojectapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,59 +15,18 @@ import okhttp3.Headers
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var rvRecipe: RecyclerView
-    private lateinit var recipeInfo: MutableList<String>
-    private lateinit var recipeImages: MutableList<String>
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-        rvRecipe = findViewById<RecyclerView>(R.id.recipeList)
-        recipeInfo = mutableListOf()
-        recipeImages = mutableListOf()
 
-        //call function
-        getRecipes()
+        val searchButton: Button = findViewById(R.id.searchButton)
+        val searchBox: EditText = findViewById(R.id.searchBox)
 
-    }
-
-    //helper function to put the name and description as one string
-    private fun getRecipeDetails(categoryObject: JSONObject): String {
-        val categoryName = categoryObject.getString("strCategory")
-        val description = categoryObject.getString("strCategoryDescription")
-        return "$categoryName\n$description"
-    }
-
-    private fun getRecipes(){
-        val client = AsyncHttpClient()
-        val apiURl = "http://www.themealdb.com/api/json/v1/1/categories.php"
-
-        client[apiURl,object : JsonHttpResponseHandler(){
-            override fun onFailure(
-                statusCode: Int,
-                headers: Headers?,
-                response: String,
-                throwable: Throwable?
-            ) {
-                Log.d("API Error", response)
-            }
-
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                val recipeArray = json.jsonObject.getJSONArray("categories")
-
-                for (i in 0 until recipeArray.length()) {
-                    val recipeObject = recipeArray.getJSONObject(i)
-                    val details = getRecipeDetails(recipeObject)
-                    recipeInfo.add(details)         //add string from helper function
-                    recipeImages.add(recipeObject.getString("strCategoryThumb")) //get image URL and add to array
-                }
-                val adapter = recipeAdapter(recipeInfo, recipeImages)  //load to adapter
-                rvRecipe.adapter = adapter
-                rvRecipe.layoutManager = LinearLayoutManager(this@MainActivity)
-                rvRecipe.addItemDecoration(DividerItemDecoration(this@MainActivity, LinearLayoutManager.VERTICAL))
-            }
-
-        }]
+        searchButton.setOnClickListener {
+            val searchText = searchBox.text.toString()
+            val intent = Intent(this, SearchResultActivity::class.java)
+            intent.putExtra("SEARCH_QUERY", searchText)
+            startActivity(intent)
+        }
     }
 }
